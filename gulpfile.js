467a9e84cf7acce018ad8921d3db2d32cff0863a
Jim Zenn <jimzenn0@gmail.com>
2017-01-23 22:25:37 -0800

    modify apollo to the skeleton aifrontiers theme

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,25 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-// 一次性编译 Sass 
+// 一次性编译 Sass
 gulp.task('sass', function() {
-    return gulp.src('./source/scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('./source/css'));
+  return gulp.src('./source/scss/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./source/css'));
+});
+
+gulp.task('install', function() {
+  gulp.src('./node_modules/jquery/dist/jquery.slim.min.*')
+    .pipe(gulp.dest('./source/js'));
+  gulp.src('./node_modules/slick-carousel/slick/slick.min.js')
+    .pipe(gulp.dest('./source/js'));
+  gulp.src('./node_modules/slick-carousel/slick/slick.scss')
+    .pipe(gulp.dest('./source/scss'));
 });
 
 // 实时编译
 gulp.task('default', ['sass'], function() {
-    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
-    gulp.watch('./source/scss/*.scss', ['sass']);
+  gulp.watch('./source/scss/_partial/*.scss', ['sass']);
+  gulp.watch('./source/scss/*.scss', ['sass']);
 });
